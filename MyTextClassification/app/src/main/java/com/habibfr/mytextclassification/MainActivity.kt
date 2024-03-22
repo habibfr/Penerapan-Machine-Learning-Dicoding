49d9f0c04e9ff272fa9601276f1f995dcdb93944
Habib Fatkhul Rohman <fatkhulrohmanhabib@gmail.com>
2024-03-22 23:00:23 +0700

    add text classification

---
+++ MyTextClassification/app/src/main/java/com/habibfr/mytextclassification/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.habibfr.mytextclassification
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.google.mediapipe.tasks.components.containers.Classifications
+import com.habibfr.mytextclassification.databinding.ActivityMainBinding
+import java.text.NumberFormat
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val textClassifierHelper = TextClassifierHelper(
+            context = this,
+            classifierListener = object : TextClassifierHelper.ClassifierListener {
+                override fun onError(error: String) {
+                    Toast.makeText(this@MainActivity, error, Toast.LENGTH_LONG).show()
+                }
+
+                override fun onResults(results: List<Classifications>?, inferenceTime: Long) {
+                    runOnUiThread {
+                        results?.let { it ->
+                            if (it.isNotEmpty() && it[0].categories().isNotEmpty()) {
+                                println(it)
+                                val sortedCategories =
+                                    it[0].categories().sortedByDescending { it?.score() }
+
+                                val displayResult =
+                                    sortedCategories.joinToString("\n") {
+                                        "${it.categoryName()} " + NumberFormat.getPercentInstance()
+                                            .format(it.score()).trim()
+                                    }
+                                binding.tvResult.text = displayResult
+                            } else {
+                                binding.tvResult.text = ""
+                            }
+                        }
+                    }
+                }
+            }
+        )
+
+        binding.btnClassify.setOnClickListener {
+            val inputText = binding.edInput.text.toString()
+            textClassifierHelper.classify(inputText)
+        }
+    }
+}
