e4e488f6e1566bdf4f9014a56035099331335164
Habib Fatkhul Rohman <fatkhulrohmanhabib@gmail.com>
2024-03-22 13:16:14 +0700

    latihan dengan gms

--- TensorFlow-Lite-Img-Classification/app/build.gradle.kts
+++ TensorFlow-Lite-Img-Classification/app/build.gradle.kts
@@ -39,7 +39,7 @@ android {
 dependencies {
 //    implementation("org.tensorflow:tensorflow-lite-support:0.1.0")
     implementation("org.tensorflow:tensorflow-lite-metadata:0.1.0")
-    implementation("org.tensorflow:tensorflow-lite-task-vision:0.4.4")
+//    implementation("org.tensorflow:tensorflow-lite-task-vision:0.4.4")
 
     val cameraxVersion = "1.3.0"
     implementation(libs.androidx.ktx)
@@ -55,4 +55,9 @@ dependencies {
     implementation("androidx.camera:camera-camera2:$cameraxVersion")
     implementation("androidx.camera:camera-lifecycle:$cameraxVersion")
     implementation("androidx.camera:camera-view:$cameraxVersion")
+
+    implementation("com.google.android.gms:play-services-tflite-support:16.1.0")
+    implementation("com.google.android.gms:play-services-tflite-gpu:16.2.0")
+    implementation("org.tensorflow:tensorflow-lite-task-vision-play-services:0.4.2")
+    implementation("org.tensorflow:tensorflow-lite-gpu:2.9.0")
 }
